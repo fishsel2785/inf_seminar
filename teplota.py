9434d4bf23d4af43e72b5d3f84d2c1dc6d7f47e4
fishsel2785 <sofia.majkutova111@gmail.com>
2026-09-21 22:42:55 +0200

    doma2

---
+++ teplota.py
@@ -0,0 +1,5 @@
+rano = int(input("aka bola ranna teplota? "))
+na_obed = int(input("aka bola teplota na obed? "))
+vecer = int(input("aka bola vecerna teplota? "))
+
+print(f"priemerna teplota bola: {(rano+na_obed+vecer)/3}")
